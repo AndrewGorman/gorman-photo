a0eeab8f40021742c7fb76cf63080e4de78de712
andrewgorman <andrew@gandrewgorman.dev>
2021-04-09 14:18:54 -0500

    Fixes vue project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
+    <div class="home">
+        <img
+            alt="Gorman Photo Logo"
+            src="@/assets/gorman_photo.svg"
+            class="logo"
+        >
         <router-view />
     </div>
 </template>
+<style
+    lang="sass"
+>
+    html,
+    body
+        max-width: 100vw
+        width: 100%
+        min-height: 100vh
+        margin: 0
+        padding: 0
+
+        .logo
+            max-width: 25rem
+            margin: 2rem
+
+        .home
+            display: flex
+            flex-direction: column
+            align-items: center
+
+
+</style>
---
+++ src/views/Gallery.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="gallery">
+        <div>photoooooosss</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Gallery',
+    };
+</script>
+<style
+    lang="sass"
+    >
+
+</style>
